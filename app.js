77275cb1eb2ec4a3fb20852297f68806356bd37d
GasinskiN <gasinskinorman@gmail.com>
2023-03-16 20:55:01 +0100

    Add get method

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const Article = require(__dirname + "/Article");
 
 const app = express();
 
-await mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+async function connectToMongoDB(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+}
+connectToMongoDB();
 
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + "/public"));
 
-
+app.get("/articles", async function(req, res){
+    try {
+        const allArticles = await Article.find(); 
+        res.send(allArticles);
+    } catch (error) {
+        res.send(error.message);
+    }
+})
 
 app.listen(3000, function(){
     console.log("Server running on port 3000");
